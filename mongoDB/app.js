52228120dea0d3918433acff6824fc8bb279fe31
Rakshit412005 <rk1056@srmist.edu.in>
2025-08-10 21:33:05 +0530

    CRUD using mongoDB

--- mongoDB/app.js
+++ mongoDB/app.js
@@ -4,5 +4,27 @@ const userModel= require('./usermodel')
 app.get('/',(req,res)=>{
     res.send("hey");
 })
+app.get('/create', async (req,res)=>{
+  let createduser = await userModel.create({
+        name:"rakshit",
+        username: "rakshit07",
+        email: "rakshitkumar0401@gmail.com"
+    })
+    res.send(createduser);
+})
+app.get('/update', async (req,res)=>{
+    let updateduser = await userModel.findOneAndUpdate(
+        {username: "rakshit07"},{name:"rakshit kumar"},{new:true}
+    )
+    res.send(updateduser);
+})
+app.get('/read', async (req,res)=>{
+    let users = await userModel.find();
+    res.send(users);
+})
+app.get('/delete', async (req,res)=>{
+    let deleteduser = await userModel.findOneAndDelete({name:"rakshit kumar"});
+    res.send(deleteduser);
+})
 console.log("server running at port 3000");
 app.listen(3000);
