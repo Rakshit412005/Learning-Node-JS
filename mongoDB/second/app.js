2984d0528c25046aa872a85b9d9cc2f30dce34b9
Rakshit412005 <rk1056@srmist.edu.in>
2025-08-11 03:50:52 +0530

    ..

--- mongoDB/second/app.js
+++ mongoDB/second/app.js
@@ -12,10 +12,21 @@ app.get('/',(req,res)=>{
     res.render("index");
 })
 app.get('/read', async (req,res)=>{
-    let allUsers = await userModel.find();
+    let users = await userModel.find();
 
-    res.render(allUsers)
+    res.render("read",{users});
 })
+app.get('/delete', async (req, res) => {
+   
+  try {
+    let deletedUsers = await userModel.deleteMany({ name: "Nishchay Kumar" });
+    res.send(`Deleted ${deletedUsers.deletedCount} users named Rakshit Kumar.`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error deleting users");
+  }
+});
+
 app.post('/create', async (req,res)=>{
     let {name,email,image}=req.body;
     let createduser= await
