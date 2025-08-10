573b58e7f8fba514a595b70b47924932c1521ccd
Rakshit412005 <rk1056@srmist.edu.in>
2025-08-11 02:29:30 +0530

    .

--- mongoDB/second/app.js
+++ mongoDB/second/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();;
 const path= require('path');
+const userModel = require('./models/user');
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -10,8 +11,20 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('/',(req,res)=>{
     res.render("index");
 })
-app.get('/read',(req,res)=>{
-    res.render('read');
+app.get('/read', async (req,res)=>{
+    let allUsers = await userModel.find();
+
+    res.render(allUsers)
+})
+app.post('/create', async (req,res)=>{
+    let {name,email,image}=req.body;
+    let createduser= await
+    userModel.create({
+        name :  name,
+        email : email,
+        image: image
+    })
+    res.send(createduser);
 })
 console.log("server running at port 3000")
 app.listen(3000);
