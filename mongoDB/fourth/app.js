e2839bf63c048376d20f465c2f31f0ee1dbc3dbb
Rakshit412005 <rk1056@srmist.edu.in>
2025-08-13 00:19:11 +0530

    /

--- mongoDB/fourth/app.js
+++ mongoDB/fourth/app.js
@@ -2,6 +2,9 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
 const path = require('path');
+const usermodel = require('./models/user');
+let bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -11,6 +14,26 @@ app.use(cookieParser());
 
 app.get('/',(req,res)=>{
     res.render("index");
+});
+
+app.post('/create',  (req,res)=>{
+    let {username,age,email,password} = req.body;
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(password,salt, async (err,hash)=>{
+              let user = await usermodel.create({
+                  password:hash,
+                  age:age,
+                  email:email,
+                  username:username
+             });
+            let token= jwt.sign({email},"secret key");
+            res.cookie("token",token);
+             res.send(user);
+
+        })
+    })
+ 
+   
 })
 
 app.listen(3000);
