6eb5a8f5876793ac96a03829f9539dc6e619db49
Rakshit412005 <rk1056@srmist.edu.in>
2025-08-12 19:32:04 +0530

    /

--- mongoDB/third/app.js
+++ mongoDB/third/app.js
@@ -1,11 +1,23 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
 app.use(cookieParser());
 app.get('/',(req,res)=>{
     res.cookie("name","Rakshit"); // send krne ke liye res use hota h
     res.send("cookie stored");
- })
+    // bcrypt.genSalt(10, function(err, salt) {
+    // bcrypt.hash("rakshit07", salt, function(err, hash) {
+    //     // Store hash in your password DB.
+    //     console.log(salt);
+    // });
+    bcrypt.compare("rakshit07","$2b$10$8kt24vv6Rh40/TBN4YF5je" , function(err, result) {
+    // result == true
+    console.log(result);
+    });
+
+});
+ //})
 app.get('/read',(req,res)=>{
     console.log(req.cookies); // read krne ke liye req use hota h
     res.send("read page");
