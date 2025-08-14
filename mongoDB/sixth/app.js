62346bf910f77cf9e4131b69d54a2d74d0321ea7
Rakshit412005 <rk1056@srmist.edu.in>
2025-08-15 01:03:28 +0530

    basic

--- mongoDB/sixth/app.js
+++ mongoDB/sixth/app.js
@@ -1,7 +1,14 @@
 const express = require ('express');
 const app = express();
+const usermodel = require('./models/user');
+const cookieParser = require('cookie-parser');
+
+app.set("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
 
 app.get('/',(res,req)=>{
-
+    res.render("index");
 })
 app.listen(3000);
