a994239c86ffc36948b4892a3edaced5e44e690d
Rakshit412005 <rk1056@srmist.edu.in>
2025-08-15 14:25:27 +0530

    ;

--- mongoDB/sixth/app.js
+++ mongoDB/sixth/app.js
@@ -15,6 +15,14 @@ app.get('/',(req,res)=>{
     res.render("index");
 })
 
+app.get('/login',(req,res)=>{
+    res.render("login");
+})
+
+app.get('/profile',isLoggedIn, (req,res)=>{
+    res.render("login");
+})
+
 app.post('/register', async (req,res)=>{
     let {email,username,name,age,password} = req.body;
     let user = await usermodel.findOne({email});
@@ -39,4 +47,41 @@ app.post('/register', async (req,res)=>{
     }
 
 })
+
+app.post('/login', async (req,res)=>{
+    let {email,password} = req.body;
+    let user = await usermodel.findOne({email});
+    if(!user){
+        return res.status(500).send("Something went wrong");
+    }
+    else{
+       bcrypt.compare(password,user.password, function(err,result){
+        if(result){
+            let token = jwt.sign({email : email, useriid : user._id}, "secret key");
+            res.cookie("token",token);
+            res.status(200).send("Logged in successfully");
+        }
+        else{
+            res.redirect('/login')
+        }
+       })
+    }
+
+})
+
+app.get('/logout',(req,res)=>{
+    res.cookie("token"," ");
+    res.redirect('/login');
+})
+function isLoggedIn(req,res,next){
+    if(req.cookies.token === ""){
+        res.send("You must log in first");
+    }
+    else{
+        let data = jwt.verify(req.cookies.token,"secret key");
+        req.user = data;
+        next();
+    }   
+}
+
 app.listen(3000);
